af62ff8c984993f6da316dd670cd947a02c1000d
Roger Urich <rogerdurich+git@gmail.com>
2024-11-12 18:41:39 -0800

    put back dotenv

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 def hello_world(request):
     name = os.environ.get('NAME')
